e4e25b3dafbc88b606a9c78931d8dc97f4859982
SilentSKY <SilentSKY@SilentSky-PC>
2020-06-03 14:36:04 +0530

    Unicode

---
+++ 09Unicode/UnicodeType.java
@@ -0,0 +1,41 @@
+class UnicodeType
+{
+	public static void main(String[] args) 
+	{
+	
+     System.out.println("\u0049 \u004C\u004F\u0056\u0045 \u0059\u004F\u0055");
+   System.out.println("\u0049 \u004C\u004F\u0056\u0045 \u0059\u004F\u0055");
+
+				System.out.println(\u0031);
+				System.out.println(\u0032);
+				System.out.println(\u0033);
+				System.out.println(\u0034);
+
+				System.out.println(\u0036);
+				System.out.println(\u0037);
+				System.out.println(\u0038);
+				System.out.println(\u0039);
+				System.out.println();
+				System.out.println(\u0031\u0032);
+				System.out.println(\u00312\u0033);
+				System.out.println(1\u00312\u0034);
+								//System.out.println(\u0041);
+								System.out.println('\u0041');
+								System.out.println('\u0042');
+								System.out.println('\u004A');
+
+          				//System.out.println(\u0041\u0042);
+          				System.out.println("\u0041\u0042");
+          				System.out.println("\u0041\u0042");
+          				//System.out.println(\u0061);
+						int a=5;
+						System.out.println(a);
+						System.out.println(\u0061);
+						System.out.println('\u0061');
+                      		\u0061=7;
+													System.out.println(a);
+							System.out.println("\u0024\u0040\u0021");
+
+			
+	}
+}
